4edfadeffdbf742fa7733527697f20955e54d8ef
mgalante <mgalante@opentrends.net>
2017-04-27 16:28:01 +0200

    Stored in sd & checked if exists

--- app/src/main/java/com/example/mgalante/mysummerapp/adapter/GalleryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/mgalante/mysummerapp/adapter/GalleryRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.mgalante.mysummerapp.adapter;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Environment;
@@ -24,6 +25,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.orhanobut.logger.Logger;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created by mgalante on 20/04/17.
@@ -34,6 +36,8 @@ public class GalleryRecyclerViewAdapter extends FirebaseRecyclerAdapter<ImageMod
     private ClickListenerGallery mClickListenerGallery;
     private Context context;
     private String userId;
+    private SharedPreferences preferences;
+    private Boolean downloadImage;
 
 
     public GalleryRecyclerViewAdapter(Context context, DatabaseReference ref, String uid, ClickListenerGallery mClickListenerGallery) {
@@ -41,6 +45,8 @@ public class GalleryRecyclerViewAdapter extends FirebaseRecyclerAdapter<ImageMod
         this.context = context;
         this.userId = uid;
         this.mClickListenerGallery = mClickListenerGallery;
+        this.preferences = context.getSharedPreferences("appPreferences", Context.MODE_PRIVATE);
+        this.downloadImage = false;
     }
 
     @Override
@@ -75,7 +81,7 @@ public class GalleryRecyclerViewAdapter extends FirebaseRecyclerAdapter<ImageMod
             mClickListenerGallery.clickImageGallery(v, position, file.getUserModel().getName(), file.getUserModel().getPhotoUrl(), file.getFileModel().getUrl_file());
         }
 
-        public void setIvGalleryPhoto(ImageModel imageModel) {
+        public void setIvGalleryPhoto(final ImageModel imageModel) {
             if (ivGalleryPhoto == null) return;
             Log.i("TEST setGALLERY", imageModel.getFileModel().getUrl_file());
             String filePath = imageModel.getFileModel().getUrl_file();
@@ -83,24 +89,29 @@ public class GalleryRecyclerViewAdapter extends FirebaseRecyclerAdapter<ImageMod
             // 1º IF IMAGE WAS SENT BY USER
             if (imageModel.getUserModel().getUid().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
 
-                //File file = new File(Environment.getExternalStorageDirectory() + Util.FOLDER_SD_IMAGES + imageModel.getFileModel().getName_file());
+                //region sent by user
                 File file = new File(Environment.getExternalStorageDirectory() + Util.FOLDER_SD_IMAGES + imageModel.getFileModel().getName_file());
+                // 1.1 SEARCH FOR IMAGE IN FILES
                 if (file.exists()) {
                     Logger.d("User File exists:" + file.getAbsolutePath());
+                    // 1.1.1 IF FOUND -> DISPLAY
                     filePath = file.getAbsolutePath();
                 } else {
                     Logger.d("User File does not exists:" + file.getAbsolutePath());
+                    // 1.1.2 ELSE -> ? shit
 
                 }
-                // 1.1 SEARCH FOR IMAGE IN FILES
 
-                // 1.1.1 IF FOUND -> DISPLAY
-                // 1.1.2 ELSE -> ? shit
+
+
+
+
+                //endregion
 
             }
-            // 2 IF IMAGES WASN'T SENT BY USER
+            // 2 IF IMAGE WASN'T SENT BY USER
             else {
-
+                //region No sent by user
                 // 2.1 SEARCH FOR IMAGE IN "App Folder" FILES
                 File file = new File(Environment.getExternalStorageDirectory() + Util.FOLDER_SD_IMAGES + imageModel.getFileModel().getName_file());
                 if (file.exists()) {
@@ -108,41 +119,54 @@ public class GalleryRecyclerViewAdapter extends FirebaseRecyclerAdapter<ImageMod
                     Logger.d("Non User File exists:" + file.getAbsolutePath());
                     filePath = file.getAbsolutePath();
                 } else {
-                    // 2.1.2 IF NOT   -> ASK FOR DOWNLOAD  -> Display a thumbnail
+                    // 2.1.2 IF NOT   -> ASK FOR DOWNLOAD
                     Logger.d("Non User File does not exists:" + file.getAbsolutePath());
-
+                    if (preferences.getBoolean(context.getString(R.string.preference_store_image_gallery), true)) {
+                        downloadImage = true;
+                    }
                 }
 
+                //Glide.with(context).load(imageModel.getFileModel().getUrl_file()).asBitmap().thumbnail(0.1f) // display the original image reduced to 10% of the size
+                Glide.with(context).load(filePath).asBitmap().thumbnail(0.1f) // display the original image reduced to 10% of the size
+                        .into(new SimpleTarget<Bitmap>() {
+
+                            @Override
+                            public void onResourceReady(final Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                                //TODO STORE IF REQUIRED
+                                //CacheStore.getInstance().saveCacheFile(uid, resource);
+                                ivGalleryPhoto.setImageBitmap(resource);
+
+                                if (downloadImage) {
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+
+                                            File file = new File(
+                                                    Environment.getExternalStorageDirectory() + Util.FOLDER_SD_IMAGES
+                                                            + imageModel.getFileModel().getName_file());
+                                            try {
+                                                file.createNewFile();
+                                                FileOutputStream ostream = new FileOutputStream(file);
+                                                resource.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+                                                ostream.close();
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }).start();
+                                }
+                            }
+
+                            @Override
+                            public void onLoadFailed(Exception e, Drawable errorDrawable) {
+
+                            }
+                        });
+//endregion
             }
             Logger.d("GlideImage loaded from: " + filePath);
 
-            //Glide.with(context).load(imageModel.getFileModel().getUrl_file()).asBitmap().thumbnail(0.1f) // display the original image reduced to 10% of the size
-            Glide.with(context).load(filePath).asBitmap().thumbnail(0.1f) // display the original image reduced to 10% of the size
-                    .into(new SimpleTarget<Bitmap>() {
-
-                        @Override
-                        public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
-                            //TODO STORE IF REQUIRED
-                            //CacheStore.getInstance().saveCacheFile(uid, resource);
-                            ivGalleryPhoto.setImageBitmap(resource);
-                        }
-
-                        @Override
-                        public void onLoadFailed(Exception e, Drawable errorDrawable) {
-
-                        }
-                    });
-
             ivGalleryPhoto.setOnClickListener(this);
-
-          /*  FileModel fileModel = imageModel.getFileModel();
-            String id = imageModel.getTimeStamp();
-            if (id != null) {
-                //DataBaseFileModel(int id, String type, String url_file, String name_file, String size_file)
-                DataBaseFileModel dataBaseFileModel = new DataBaseFileModel(Integer.parseInt(id.substring(id.length() - 9)), fileModel.getType(), fileModel.getUrl_file(), fileModel.getName_file(), fileModel.getSize_file());
-                dataBaseFileModel.toString();
-                dataBaseFileModel.save();
-            }*/
         }
     }
 }
